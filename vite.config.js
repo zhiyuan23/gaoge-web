8251fe209847366126d60e3db06cebacc3c453b9
snowsun <snowsun@ehsure.com>
2026-03-06 17:46:34 +0800

    ci: 🎡 集成配置修改

--- vite.config.js
+++ vite.config.js
@@ -1,10 +1,7 @@
 import { defineConfig } from 'vite'
 import vue from '@vitejs/plugin-vue'
 
-const repoName = process.env.GITHUB_REPOSITORY?.split('/')[1] || ''
-const base = process.env.GITHUB_ACTIONS ? `/${repoName}/` : '/'
-
 export default defineConfig({
   plugins: [vue()],
-  base
+  base: '/',
 })
